b21e9d7bbf9b9566185fac1642ef6de1427ce629
Rachelle A. Spicer <spicer.rachelle@gmail.com>
2020-09-16 19:23:13 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,11 +1,12 @@
 def square_array(numbers)
-  count = 0
-  while count < numbers.length 
-    numbers.index ** 2 
-    count += 1
-  end
-  square_array(numbers)
-end 
+  new_array = []
+  counter = 0 
+  while counter < numbers.length()
+  new_array.push(numbers[counter] * numbers[counter])
+  counter += 1
+  end 
+  return new_array
+end
  
 
 
